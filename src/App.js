d28f281e40fcfb514aa7b4d2ff074684d64073bf
Stheven Cabral <stheven.cabral@gmail.com>
2020-06-22 23:34:16 -0700

    Update App.js
    -Setup app.js state and fetch courses to test express api.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    fetchCourses();
+  });
+
+  const [courseObjects, setCourses] = useState([]);
+  
+  const fetchCourses = async () => {
+    const data = await fetch('localhost:5000/api/courses');
+    const courses = await data.json();
+    setCourses(courses.courses);
+  }; 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
