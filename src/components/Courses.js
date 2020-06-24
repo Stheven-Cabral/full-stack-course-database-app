b62da2358cce4f56f3f59c6ee5a79da38ddbfd4a
Stheven Cabral <stheven.cabral@gmail.com>
2020-06-23 21:12:19 -0700

    Update Courses.js

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -16,9 +16,8 @@ class Courses extends Components {
           <div className="bounds">
             <h1 className="header--logo">Courses</h1>
             <nav>
-              
-              <a className="signup" href="sign-up.html">Sign Up</a>
-              <a className="signin" href="sign-in.html">Sign In</a>
+              <Link className="signup" to='sign-up.html'>Sign Up</Link>
+              <Link className="signin" to='sign-in.html'>Sign Up</Link>
             </nav>
           </div>
         </div>
@@ -27,21 +26,26 @@ class Courses extends Components {
 
         <div className="bounds">
           <div className="grid-33">
-            <a className="course--module course--link" href="course-detail.html">
+            <Link className="course--module course--link" to="course-detail.html">
               <h4 className="course--label">Course</h4>
               <h3 className="course--title">Build a Basic Bookcase</h3>
-            </a>
+            </Link>
           </div>
 
-          <div className="grid-33"><a className="course--module course--add--module" href="create-course.html">
+          <div className="grid-33">
+            <Link className="course--module course--add--module" to="create-course.html">
               <h3 className="course--add--title">
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
                   <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-                </svg> New Course</h3>
-            </a>
+                </svg>
+                New Course
+              </h3>
+            </Link>
           </div>
         </div>
       </div>
     )
   }
 }
+
+export default Courses;
