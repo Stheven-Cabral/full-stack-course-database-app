8c86dd36d4850fe027316c24569e6af179910d02
Stheven Cabral <stheven.cabral@gmail.com>
2020-06-29 08:34:46 -0700

    06292020 0834

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,18 @@ import Header from './components/Header';
 import Courses from './components/Courses';
 import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
+import CourseDetail from './components/CourseDetail';
+import CreateCourse from './components/CreateCourse';
+import ErrorPage from './components/ErrorPage';
+import Forbidden from './components/Forbidden';
+import NotFound from './components/NotFound';
 
 const HeaderWithContext = withContext(Header);
 const CoursesWithContext = withContext(Courses);
 const UserSignInWithContext = withContext(UserSignIn);
 const UserSignUpWithContext = withContext(UserSignUp);
+const CourseDetailWithContext = withContext(CourseDetail);
+const CreateCourseWithContext = withContext(CreateCourse);
 
 export default () => (
     <Router>
@@ -25,6 +32,11 @@ export default () => (
           <Route path="/courses" component={CoursesWithContext} />
           <Route path="/signin" component={UserSignInWithContext} />
           <Route path="/signup" component={UserSignUpWithContext} />
+          <Route path="/course-detail" component={CourseDetailWithContext} />
+          <Route path="/create-course" component={CreateCourseWithContext} />
+          <Route path="/error" component={ErrorPage} />
+          <Route path="/forbidden" component={Forbidden} />
+          <Route path="/notfound" component={NotFound} />
         </Switch>
       </div>
     </Router>
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -21,7 +21,7 @@ export default class Courses extends Component {
         <div className="bounds">
           <div className="grid-33">
             {this.state.courses.map(course => 
-              <Link className="course--module course--link" to="course-detail.html">
+              <Link className="course--module course--link" to="/course-detail">
               <h4 className="course--label">Course</h4>
               <h3 className="course--title">{course.title}</h3>
             </Link>
@@ -29,7 +29,7 @@ export default class Courses extends Component {
           </div>
 
           <div className="grid-33">
-            <Link className="course--module course--add--module" to="create-course.html">
+            <Link className="course--module course--add--module" to="/create-course">
               <h3 className="course--add--title">
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
                   <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
